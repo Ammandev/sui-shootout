2cd34c7aebd732e760667ed8e270a4dfdb66e604
Muhammad Amman <developercity316@gmail.com>
2024-10-05 19:25:57 +0500

    intial

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import './App.css';
+import { Unity, useUnityContext } from "react-unity-webgl";
+import { useEffect, useState } from "react";
+
+function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/TrumpRunner.loader.js",
+    dataUrl: "build/TrumpRunner.data",
+    frameworkUrl: "build/TrumpRunner.framework.js",
+    codeUrl: "build/TrumpRunner.wasm",
+  });
+
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="App">
+      <Unity
+        style={{
+          width: `${windowDimensions.width}px`,
+          height: `${windowDimensions.height}px`,
+        }}
+        unityProvider={unityProvider}
+      />
+    </div>
+  );
+}
+
+export default App;
