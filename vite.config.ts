1b6b795a9296e08bf5a199ec32013cc56cbd2be4
Muhammad Amman <developercity316@gmail.com>
2024-10-06 01:19:22 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/TelegramBot/', // Set this to your repository name
+  base: '/Telegram-mini-app/', // Set this to your repository name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
